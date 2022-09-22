bd4fdaf992f9a2759dbe22d5a5cb169a3d735dc5
antheiz <salupacode@gmail.com>
2022-09-22 19:52:37 +0700

    Add environtments structure directory and deployment strategy

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config
+from config import Config
+import os
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app(config_class=Config):
+
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(os.getenv("APP_SETTINGS", "config.DevelopmentConfig"))
 
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ config.py
@@ -0,0 +1,28 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
+
+DATABASE_URI = os.getenv("DATABASE_URL")
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', default='014145ea5815464ebca08240f2b8bbc6')
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
