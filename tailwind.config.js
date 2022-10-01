ac8d616e1bd875406e0c526eb9462a25d10eed03
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-10-02 00:34:05 +0700

    feat: Update resource from CDN to Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/src/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+          poppins: 'Poppins',
+      }
+    }
+  },
+  darkMode: 'class',
+  
+  plugins: [],
+}
